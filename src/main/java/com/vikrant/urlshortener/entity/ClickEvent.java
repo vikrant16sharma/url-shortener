c1687867fcc0fa78c87b6d2073425204fb361458
vikrant16sharma <s.vikrant3002@gmail.com>
2026-09-26 15:19:59 +0530

    perf: optimize click analytics queries

--- src/main/java/com/vikrant/urlshortener/entity/ClickEvent.java
+++ src/main/java/com/vikrant/urlshortener/entity/ClickEvent.java
@@ -5,7 +5,15 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "click_events")
+@Table(
+        name = "click_events",
+        indexes = {
+                @Index(
+                        name = "idx_click_events_url_clicked_at",
+                        columnList = "url_id, clicked_at"
+                )
+        }
+)
 public class ClickEvent {
 
     @Id
